a4f1370c63ce48a69202f18f4d6dbc70e3c55a95
Joakim Rosenfeldt Pedersen <joakim@mellonn.com>
2024-01-11 12:27:48 +0100

    added ably messages to put and post actions

--- api/testDB.go
+++ api/testDB.go
@@ -1,11 +1,14 @@
 package handler
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/ably/ably-go/ably"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -16,6 +19,7 @@ type Message struct {
 }
 
 func TestDB(w http.ResponseWriter, r *http.Request) {
+	// Creating connection to DB
 	dbUsername := os.Getenv("PLANETSCALE_USERNAME")
 	dbPassword := os.Getenv("PLANETSCALE_PASSWORD")
 	dataSourceName := dbUsername + ":" + dbPassword + "@tcp(aws.connect.psdb.cloud)/speak?tls=true&interpolateParams=true"
@@ -65,7 +69,7 @@ func TestDB(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
 		} else {
-			log.Printf("Responded with: %v", jsonResp)
+			log.Printf("Responded with: %v", string(jsonResp[:]))
 			w.WriteHeader(http.StatusCreated)
 			w.Header().Set("Content-Type", "application/json")
 			w.Write(jsonResp)
@@ -88,18 +92,77 @@ func TestDB(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
 		}
-		resp := make(map[string]string)
+
+		resp := make(map[string]interface{})
 		resp["message"] = "Test was created!"
+		resp["object"] = message
 		jsonResp, err := json.Marshal(resp)
 		if err != nil {
 			log.Printf("Error while marshalling response: %v", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
 		} else {
-			log.Printf("Responded with: %v", jsonResp)
+			log.Printf("Responded with: %v", string(jsonResp[:]))
+			sendMessage("created", string(jsonResp[:]))
 			w.WriteHeader(http.StatusCreated)
 			w.Header().Set("Content-Type", "application/json")
 			w.Write(jsonResp)
 		}
 	}
+
+	if r.Method == "PUT" {
+		var message Message
+		err := json.NewDecoder(r.Body).Decode(&message)
+		if err != nil {
+			log.Printf("Error while getting message: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+		}
+
+		_, err = db.Exec("UPDATE Test SET title = ? WHERE id = ?;", message.Title, message.ID)
+		if err != nil {
+			log.Printf("Error while inserting message in db: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+
+		resp := make(map[string]interface{})
+		resp["message"] = "Test was updated!"
+		resp["object"] = message
+		jsonResp, err := json.Marshal(resp)
+		if err != nil {
+			log.Printf("Error while marshalling response: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		} else {
+			log.Printf("Responded with: %v", string(jsonResp)[:])
+			sendMessage("updated", string(jsonResp[:]))
+			w.WriteHeader(http.StatusCreated)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(jsonResp)
+		}
+	}
+}
+
+func sendMessage(topic string, message string) {
+	// Creating connection to ably
+	ablyKey := os.Getenv("ABLY_API_KEY")
+	client, err := ably.NewRealtime(ably.WithKey(ablyKey), ably.WithAutoConnect(false))
+	if err != nil {
+		log.Printf("Error while connecting to ably: %v", err)
+	}
+	client.Connection.OnAll(func(change ably.ConnectionStateChange) {
+		log.Printf("Connection event: %s state=%s reason=%s\n", change.Event, change.Current, change.Reason)
+	})
+	client.Connection.On(ably.ConnectionEventClosed, func(change ably.ConnectionStateChange) {
+		log.Println("Closed the connection to Ably.")
+	})
+	client.Connect()
+
+	channel := client.Channels.Get("test")
+	err = channel.Publish(context.Background(), topic, message)
+	if err != nil {
+		log.Printf("Error while sending message: %v", err)
+	}
+	client.Close()
 }
