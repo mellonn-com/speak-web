0221476fb7b8964ae394e0f516cc94e64d028e03
Joakim Rosenfeldt Pedersen <joakim@mellonn.com>
2024-01-11 12:28:06 +0100

    added ably listener

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module speak-web
 
 go 1.21.0
 
-require github.com/go-sql-driver/mysql v1.7.1 // indirect
+require (
+	github.com/ably/ably-go v1.2.14 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/klauspost/compress v1.17.4 // indirect
+	github.com/ugorji/go/codec v1.2.12 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	nhooyr.io/websocket v1.8.10 // indirect
+)
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,61 @@
 <script lang="ts">
+	import * as Ably from 'ably';
+
 	interface Message {
 		ID: number;
 		Title: string;
 	}
 
-	async function fetchData(): Promise<Message[]> {
+	let messages: Message[] = [];
+
+	async function setupPage() {
+		await fetchData();
+		await subscribeToAbly();
+	}
+
+	async function subscribeToAbly() {
+		const KEY = (await (await fetch('/api/keys')).json()).key;
+		console.log(KEY);
+		const ably = new Ably.Realtime.Promise({ key: KEY });
+		await ably.connection.once('connected');
+		console.log('Connected to ably');
+		const channel = ably.channels.get('test');
+
+		channel.subscribe((message) => {
+			console.log(`Message received from ably: ${message}`);
+			const newElement = JSON.parse(message.data).object as Message;
+			console.log(`newElement: ${JSON.stringify(newElement)}`);
+			console.log(`Checking against list: ${JSON.stringify(messages)}`);
+			let messageToChange = messages.findIndex((message) => message.ID == newElement.ID);
+			if (messageToChange == -1) {
+				console.log(`Failed getting element from list, with id: ${newElement.ID}`);
+				fetchData();
+				return;
+			}
+			messages[messageToChange].Title = newElement.Title;
+			console.log('Updated message!');
+		});
+
+		window.addEventListener('beforeunload', async function () {
+			await channel.detach();
+			ably.close();
+		});
+	}
+
+	async function fetchData() {
 		let response = await fetch(`/api/testDB`);
 		console.log(response);
-		return (await response.json()).messages;
+		messages = (await response.json()).messages;
 	}
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 
-{#await fetchData()}
+{#await setupPage()}
 	<p>loading</p>
-{:then items}
-	{#each items as item}
-		<li>{item.ID}. {item.Title}</li>
+{:then}
+	{#each messages as message}
+		<li>{message.ID}. {message.Title}</li>
 	{/each}
 {:catch error}
 	<p style="color: red">{error.message}</p>
