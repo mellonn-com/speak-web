c010c7c3c0752a0da04d9cbd18012d250bb38e15
Joakim Rosenfeldt Pedersen <joakim@mellonn.com>
2024-06-12 16:57:28 +0200

    fixed some issue

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const isProd = process.env.NODE_ENV !== 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -11,6 +13,9 @@ const config = {
         adapter: adapter(),
         alias: {
             'schema': 'src/db/schema'
+        },
+        csrf: {
+            checkOrigin: isProd
         }
     }
 };
